0948cf1c859e274a000198d7cb6b62709fe00280
Tokyo <ahmed.tokyo1@gmail.com>
2017-02-14 17:04:59 +0200

    seperate who-am-i-listener

--- src/listeners/listeners.js
+++ src/listeners/listeners.js
@@ -48,18 +48,8 @@ module.exports = function(controller) {
     });
   });
 
-  controller.hears([
-    'what is my name', 'who am i',
-  ], 'direct_message,direct_mention,mention', (bot, message) => {
-    controller.storage.users.get(message.user, (err, user) => {
-      if (user && user.name) {
-        bot.reply(message, 'Your name is ' + user.name);
-      } else {
-        Conversations.whatShouldICallYouConvo(message);
-      }
-    });
-  });
-
+  // who-am-i-listener listener
+  require('./who-am-i-listener')(controller);
   // testing listener
   require('./testruru-listener')(controller);
   // grocery-list listener
---
+++ src/listeners/who-am-i-listener.js
@@ -0,0 +1,17 @@
+'usestrict';
+
+const Conversations = require('../conversations/conversations');
+
+module.exports = function (controller){
+  controller.hears([
+    'what is my name', 'who am i',
+  ], 'direct_message,direct_mention,mention', (bot, message) => {
+    controller.storage.users.get(message.user, (err, user) => {
+      if (user && user.name) {
+        bot.reply(message, 'Your name is ' + user.name);
+      } else {
+        Conversations.whatShouldICallYouConvo(message);
+      }
+    });
+  });
+};
