57b740cacd3c8d88585019647bbffd1a1e866109
Tokyo <ahmed.tokyo1@gmail.com>
2017-02-14 17:17:26 +0200

    update config.js

--- config/config.js
+++ config/config.js
@@ -1,3 +1,12 @@
 module.exports = {
   MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/rurubot',
+  CLEVERBOT_API_USER: process.env.CLEVERBOT_API_USER,
+  CLEVERBOT_API_KEY: process.env.CLEVERBOT_API_KEY,
+  SALCKBOT_TOKEN: process.env.SALCKBOT_TOKEN,
+  SALCKBOT_API_TOKEN: process.env.SALCKBOT_API_TOKEN,
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  VERIFICATION_TOKEN: process.env.VERIFICATION_TOKEN,
+  WORDSAPI_API_KEY: process.env.WORDSAPI_API_KEY,
+  PORT: 4820
 };
