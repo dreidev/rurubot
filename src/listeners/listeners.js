4630aaf7a91aac600c4c20fd07236f1f1fbf8021
Tokyo <ahmed.tokyo1@gmail.com>
2017-02-14 17:13:28 +0200

    modularized listeners

--- src/listeners/listeners.js
+++ src/listeners/listeners.js
@@ -1,39 +1,17 @@
 'usestrict';
-
-// const config = require('../../config/config');
-// const jsonQuery = require('json-query');
-const API = require('../api');
-const Conversations = require('../conversations/conversations');
-
-// dreidev basic data
-const basicDataJSON = require('../../data/basic-data.json');
-
 module.exports = function(controller) {
-  controller.hears([
-    'call me (.*)', 'my name is (.*)',
-  ], 'direct_message,direct_mention,mention', (bot, message) => {
-    let name = message.match[1];
-    controller.storage.users.get(message.user, (err, user) => {
-      if (!user) {
-        user = {
-          id: message.user,
-        };
-      }
-      user.name = name;
-      controller.storage.users.save(user, (err, id) => {
-        bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-      });
-    });
-  });
-
-  // pants-listener listener
-  require('./private/pants-listener')(controller);
+  // my-name-is listener
+  require('./my-name-is-listener')(controller);
   // who-am-i-listener listener
   require('./who-am-i-listener')(controller);
   // testing listener
   require('./testruru-listener')(controller);
   // grocery-list listener
   require('./grocery-list-listener')(controller);
+  // private listeners
+  // pants-listener listener
+  require('./private/pants-listener')(controller);
+  // FALLBACKS
   // FALLBACK to cleverbot
   require('./fallback-cleverbot-listener')(controller);
 };
---
+++ src/listeners/my-name-is-listener.js
@@ -0,0 +1,20 @@
+'usestrict';
+
+module.exports = function (controller){
+  controller.hears([
+    'call me (.*)', 'my name is (.*)',
+  ], 'direct_message,direct_mention,mention', (bot, message) => {
+    let name = message.match[1];
+    controller.storage.users.get(message.user, (err, user) => {
+      if (!user) {
+        user = {
+          id: message.user,
+        };
+      }
+      user.name = name;
+      controller.storage.users.save(user, (err, id) => {
+        bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+      });
+    });
+  });
+};
