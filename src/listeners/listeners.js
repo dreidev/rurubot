88ed73aa448e91af4111a76f104c0a02f783d120
Tokyo <ahmed.tokyo1@gmail.com>
2017-02-14 17:09:10 +0200

    seperated pants listener

--- src/listeners/listeners.js
+++ src/listeners/listeners.js
@@ -26,28 +26,8 @@ module.exports = function(controller) {
     });
   });
 
-  // pants function
-  controller.hears([
-    'what does nader miss (.*)', 'what do I miss (.*)',
-  ], 'direct_message,direct_mention,mention', (bot, message) => {
-    let name = message.match[1];
-    controller.storage.users.get(message.user, (err, user) => {
-      if (!user) {
-        user = {
-          id: message.user,
-        };
-      }
-      user.name = name;
-      API.getMemberInfo(user.id).then((response) => {
-        if (basicDataJSON.dreidevInnerCircleUNames.indexOf(response.data.user.name) > -1) {
-          bot.reply(message, 'T h e pants !! ');
-        }
-      }).catch((error) => {
-        console.log(error);
-      });
-    });
-  });
-
+  // pants-listener listener
+  require('./private/pants-listener')(controller);
   // who-am-i-listener listener
   require('./who-am-i-listener')(controller);
   // testing listener
