bb2f8a3477ae00f0d60b8851ac430c4c930b3a6f
Tokyo <ahmed.tokyo1@gmail.com>
2017-02-16 03:20:25 +0200

    server: add projects route

--- src/server/routes/project.js
+++ src/server/routes/project.js
@@ -7,4 +7,20 @@ const Project = require('../../models/project');
 // const axios = require('axios');
 // const querystring = require('querystring');
 
+/*
+|--------------------------------------------------------------------------
+| Get all projects
+|--------------------------------------------------------------------------
+*/
+router.get('/api/projects', (req, res, next) => {
+  Project.find().then((project) => {
+    if (!project) {
+      return res.status(404).send({message: 'not found'});
+    }
+    res.send(project);
+  }).catch(function(error) {
+    next(error);
+  });
+});
+
 module.exports = router;
