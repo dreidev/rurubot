2311e0d5e7ca04cea7acfe9e2397ff4e7141b999
Tokyo <ahmed.tokyo1@gmail.com>
2017-02-18 05:42:29 +0200

    server: Create Email and Password Account

--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 //
 // const config = require('../config/config');
 const User = require('../models/user');
-// const Auth = require('../routes/authentication');
+const Auth = require('../routes/authentication');
 // const axios = require('axios');
 // const querystring = require('querystring');
 
@@ -77,74 +77,38 @@ router.get('/api/users/slack/:id', (req, res, next) => {
 //  |--------------------------------------------------------------------------
 //  */
 // router.post('/api/auth/login', function(req, res) {
-//   User.findOne({ email: req.body.email }, '+password', function(err, user) {
+//   User.findOne({email: req.body.email }, '+password', function(err, user) {
 //     if (!user) {
-//       return res.status(401).send({ message: 'Invalid email and/or password' });
+//       return res.status(401).send({message: 'Invalid email and/or password' });
 //     }
 //     user.comparePassword(req.body.password, function(err, isMatch) {
 //       if (!isMatch) {
-//         return res.status(401).send({ message: 'Invalid email and/or password' });
+//         return res.status(401).send({message: 'Invalid email and/or password' });
 //       }
-//       res.send({ token: Auth.createJWT(user) });
+//       res.send({token: Auth.createJWT(user) });
 //     });
 //   });
 // });
 //
-// /*
-//  |--------------------------------------------------------------------------
-//  | Create Email and Password Account
-//  |--------------------------------------------------------------------------
-//  */
-// router.post('/api/auth/signup', function(req, res) {
-//   User.findOne({ email: req.body.email }, function(err, existingUser) {
-//     if (existingUser) {
-//       return res.status(409).send({ message: 'Email is already taken' });
-//     }
-//     var user = new User(req.body);
-//     user.profile_photo = `/static/user-profile-default.svg`
-//     user.save(function(err, result) {
-//       if (err) {
-//         res.status(500).send({ message: err.message });
-//       }
-//       res.send({ token: Auth.createJWT(result) });
-//     });
-//   });
-// });
-//
-// router.post('/api/auth/facebook', function(req, res, next) {
-//   var fields = ['id', 'email', 'first_name', 'last_name', 'link', 'name'];
-//   var accessTokenUrl = 'https://graph.facebook.com/v2.8/oauth/access_token';
-//   var graphApiUrl = 'https://graph.facebook.com/v2.8/me?fields=' + fields.join(',');
-//   var params = {
-//     code: req.body.code,
-//     client_id: req.body.clientId,
-//     client_secret: config.FACEBOOK_SECRET,
-//     redirect_uri: req.body.redirectUri
-//   };
-//   axios.get(accessTokenUrl, {
-//     params
-//   }).then(function(response){
-//     return axios.get(graphApiUrl, {
-//       params: {access_token : response.data.access_token}
-//     })
-//   })
-//   .then(response=>{
-//     let { email, first_name, last_name} = response.data
-//     return User.findOne({email}).then(user=>{
-//       if (user) {
-//         return user
-//       } else{
-//         return User.create({
-//           email, first_name, last_name
-//         })
-//       }
-//     })
-//   })
-//   .then(user=>res.send({ token: Auth.createJWT(user) }))
-//   .catch(error=>{
-//     console.log( error);
-//     next(error)
-//   })
-// });
+/*
+ |--------------------------------------------------------------------------
+ | Create Email and Password Account
+ |--------------------------------------------------------------------------
+ */
+router.post('/api/auth/signup', function(req, res) {
+  User.findOne({email: req.body.email}, (err, existingUser) => {
+    if (existingUser) {
+      return res.status(409).send({message: 'Email is already taken'});
+    }
+    let user = new User(req.body);
+    // user.profile_photo = `/static/user-profile-default.svg`
+    user.save(function(err, result) {
+      if (err) {
+        res.status(500).send({message: err.message});
+      }
+      res.send({token: Auth.createJWT(result)});
+    });
+  });
+});
 
 module.exports = router;
