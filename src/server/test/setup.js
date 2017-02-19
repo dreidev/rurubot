d3ca966e9b26a21984b23493bf9c3279f8ca1a3b
Tokyo <ahmed.tokyo1@gmail.com>
2017-02-19 16:17:15 +0200

    server: add basic test setup file

---
+++ src/server/test/setup.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const {MONGO_TEST_URI} = require('../config/config');
+mongoose.Promise = Promise;
+mongoose.connection.on('error', function() {
+  console.log('error');
+});
+
+function clearDB(done) {
+  mongoose.connection.collections.forEach((i) => {
+    mongoose.connection.collections[i].remove(()=>{});
+  });
+  return done();
+}
+
+// eslint-disable-next-line
+before(function(done) {
+  if (mongoose.connection.readyState === 0) {
+    mongoose.connect(MONGO_TEST_URI, function(err) {
+      if (err) {
+        throw err;
+      }
+      return clearDB(done);
+    });
+  } else {
+    clearDB(done);
+  }
+});
+
+// eslint-disable-next-line
+after(function(done) {
+  mongoose.disconnect();
+  return done();
+});
+
+exports.clearDB = clearDB;
