8d396fda83e5f5c4c784fd41fd3b6d95fdc50aec
Tokyo <ahmed.tokyo1@gmail.com>
2017-02-19 16:06:42 +0200

    server: add basic test directiry

---
+++ src/server/test/utils.js
@@ -0,0 +1,32 @@
+const User = require('../models/user');
+
+exports.createAUser = ({
+    email = 'adel.zee@gmail.com',
+    password = '12345',
+  }) => {
+    return User.create(
+  {
+    slack_id: 'U0U05G6JZ',
+    slack_team_id: 'T04NXFZPN',
+    email: email,
+    password: password,
+    name: 'tokyo',
+    deleted: false,
+    real_name: 'Ahmed Tokyo',
+    // This follows profile in slack schema.
+    profile: {
+      first_name: 'Ahmed',
+      last_name: 'Tokyo',
+      email: email,
+      title: 'Front End Engineer',
+      phone: '01124711175',
+    },
+    is_admin: true,
+    is_owner: false,
+    is_primary_owner: false,
+    is_restricted: false,
+    is_ultra_restricted: false,
+    is_bot: false,
+    has_2fa: false,
+  });
+};
