a491964218d28053596e7e277330d8cf78b5eb0f
Tokyo <ahmed.tokyo1@gmail.com>
2017-02-20 03:39:17 +0200

    server: test: grocerylist tests

--- src/server/test/grocery-list-spec.js
+++ src/server/test/grocery-list-spec.js
@@ -16,18 +16,13 @@ describe(`Grocery List API`, function() {
     before((done) => {
       let user_id = `U0U05G6JZ`;
       let name = ['milk'];
-      test.post(`/api/grocery-list`).send({
-        user_id,
-        name,
-      })
-      .then(() => {
+      test.post(`/api/grocery-list`).send({user_id, name}).then(() => {
         done();
       }).catch();
     });
     // eslint-disable-next-line
     it(`should return a list of grocery list items`, () => {
-      return test.get(`/api/grocery-list`)
-        .expect(200).then(({body}) => {
+      return test.get(`/api/grocery-list`).expect(200).then(({body}) => {
         assert.isArray(body);
         assert.equal(body[0].name, 'milk');
       });
@@ -39,18 +34,13 @@ describe(`Grocery List API`, function() {
     before((done) => {
       let user_id = `U0U05G6JZ`;
       let name = ['milk'];
-      test.post(`/api/grocery-list`).send({
-        user_id,
-        name,
-      })
-      .then(() => {
+      test.post(`/api/grocery-list`).send({user_id, name}).then(() => {
         done();
       }).catch();
     });
     // eslint-disable-next-line
     it(`should return a list of unchecked grocery list items`, () => {
-      return test.get(`/api/grocery-list/state/notChecked`)
-        .expect(200).then(({body}) => {
+      return test.get(`/api/grocery-list/state/notChecked`).expect(200).then(({body}) => {
         assert.isArray(body);
         assert.equal(body[0].state, 'notChecked');
       });
@@ -63,19 +53,13 @@ describe(`Grocery List API`, function() {
       let user_id = `U0U05G6JZ`;
       let name = ['milk'];
       let state = 'checked';
-      test.post(`/api/grocery-list`).send({
-        user_id,
-        name,
-        state,
-      })
-      .then(() => {
+      test.post(`/api/grocery-list`).send({user_id, name, state}).then(() => {
         done();
       }).catch();
     });
     // eslint-disable-next-line
-    it(`should return a list of unchecked grocery list items`, () => {
-      return test.get(`/api/grocery-list/state/checked`)
-        .expect(200).then(({body}) => {
+    it(`should return a list of checked grocery list items`, () => {
+      return test.get(`/api/grocery-list/state/checked`).expect(200).then(({body}) => {
         assert.isArray(body);
         assert.equal(body[0].state, 'checked');
       });
@@ -88,19 +72,13 @@ describe(`Grocery List API`, function() {
       let user_id = `U0U05G6JZ`;
       let name = ['milk'];
       let state = 'checked';
-      test.post(`/api/grocery-list`).send({
-        user_id,
-        name,
-        state,
-      })
-      .then(() => {
+      test.post(`/api/grocery-list`).send({user_id, name, state}).then(() => {
         done();
       }).catch();
     });
     // eslint-disable-next-line
-    it(`should return a list of unchecked grocery list items`, () => {
-      return test.get(`/api/grocery-list/user/U0U05G6JZ`)
-        .expect(200).then(({body}) => {
+    it(`should return a list of grocery list items by user`, () => {
+      return test.get(`/api/grocery-list/user/U0U05G6JZ`).expect(200).then(({body}) => {
         assert.isArray(body);
         assert.equal(body[0].user_id, 'U0U05G6JZ');
       });
@@ -113,19 +91,18 @@ describe(`Grocery List API`, function() {
     before((done) => {
       let user_id = `U0U05G6JZ`;
       let name = ['milk'];
-      test.post(`/api/grocery-list`).send({
-        user_id,
-        name,
-      })
-      .then((res) => {
+      test.post(`/api/grocery-list`).send({user_id, name}).then((res) => {
         _id = res.body;
         done();
       }).catch();
     });
     // eslint-disable-next-line
-    it(`should return a list of unchecked grocery list items`, () => {
-      return test.put(`/api/grocery-list/${_id}/check`)
-        .expect(200);
+    it(`should update the state of the grocery list item to checked`, () => {
+      return test.put(`/api/grocery-list/${_id}/check`).expect(200);
+    });
+    // eslint-disable-next-line
+    it(`should update the state of the grocery list item to unchecked`, () => {
+      return test.put(`/api/grocery-list/${_id}/uncheck`).expect(200);
     });
   });
 });
