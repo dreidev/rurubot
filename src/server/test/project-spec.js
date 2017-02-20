5b0a340601727af899d75ce386c77c9c70a02064
Tokyo <ahmed.tokyo1@gmail.com>
2017-02-20 04:08:14 +0200

    server: test: wip: project

--- src/server/test/project-spec.js
+++ src/server/test/project-spec.js
@@ -11,98 +11,91 @@ describe(`Grocery List API`, function() {
   // eslint-disable-next-line
   before(clearDB);
   // eslint-disable-next-line
-  describe(`Grocery List Retrieval`, () => {
+  before((done) => {
+    let name = 'fanart';
+    let category = `web`;
+    let client = `Min Young`;
+    test.post(`/api/project`).send({name, category, client}).then(() => {
+      done();
+    }).catch();
+  });
+  // eslint-disable-next-line
+  describe(`Projects Retrieval`, () => {
     // eslint-disable-next-line
-    before((done) => {
-      let user_id = `U0U05G6JZ`;
-      let name = 'milk';
-      test.post(`/api/grocery-list`).send({user_id, name}).then(() => {
-        done();
-      }).catch();
-    });
-    // eslint-disable-next-line
-    it(`should return a list of grocery list items`, () => {
-      return test.get(`/api/grocery-list`).expect(200).then(({body}) => {
+    it(`should return a list of projects`, () => {
+      return test.get(`/api/projects`).expect(200).then(({body}) => {
         assert.isArray(body);
-        assert.equal(body[0].name, 'milk');
+        assert.equal(body[0].name, 'fanart');
       });
     });
   });
   // eslint-disable-next-line
-  describe(`Daily Tasks Retrieval by state (not checked)`, () => {
+  describe(`Projects Retrieval by name)`, () => {
     // eslint-disable-next-line
-    before((done) => {
-      let user_id = `U0U05G6JZ`;
-      let name = 'milk';
-      test.post(`/api/grocery-list`).send({user_id, name}).then(() => {
-        done();
-      }).catch();
-    });
-    // eslint-disable-next-line
-    it(`should return a list of unchecked grocery list items`, () => {
-      return test.get(`/api/grocery-list/state/notChecked`).expect(200).then(({body}) => {
+    it(`should return a list of projects with the specified name`, () => {
+      return test.get(`/api/project/name/fanart`).expect(200).then(({body}) => {
         assert.isArray(body);
-        assert.equal(body[0].state, 'notChecked');
+        assert.equal(body[0].name, 'fanart');
       });
     });
   });
-  // eslint-disable-next-line
-  describe(`Daily Tasks Retrieval by state (checked)`, () => {
-    // eslint-disable-next-line
-    before((done) => {
-      let user_id = `U0U05G6JZ`;
-      let name = 'milk';
-      let state = 'checked';
-      test.post(`/api/grocery-list`).send({user_id, name, state}).then(() => {
-        done();
-      }).catch();
-    });
-    // eslint-disable-next-line
-    it(`should return a list of checked grocery list items`, () => {
-      return test.get(`/api/grocery-list/state/checked`).expect(200).then(({body}) => {
-        assert.isArray(body);
-        assert.equal(body[0].state, 'checked');
-      });
-    });
-  });
-  // eslint-disable-next-line
-  describe(`Daily Tasks Retrieval by User`, () => {
-    // eslint-disable-next-line
-    before((done) => {
-      let user_id = `U0U05G6JZ`;
-      let name = 'milk';
-      let state = 'checked';
-      test.post(`/api/grocery-list`).send({user_id, name, state}).then(() => {
-        done();
-      }).catch();
-    });
-    // eslint-disable-next-line
-    it(`should return a list of grocery list items by user`, () => {
-      return test.get(`/api/grocery-list/user/U0U05G6JZ`).expect(200).then(({body}) => {
-        assert.isArray(body);
-        assert.equal(body[0].user_id, 'U0U05G6JZ');
-      });
-    });
-  });
-  // eslint-disable-next-line
-  describe(`Daily Tasks Update`, () => {
-    let _id;
-    // eslint-disable-next-line
-    before((done) => {
-      let user_id = `U0U05G6JZ`;
-      let name = 'milk';
-      test.post(`/api/grocery-list`).send({user_id, name}).then((res) => {
-        _id = res.body;
-        done();
-      }).catch();
-    });
-    // eslint-disable-next-line
-    it(`should update the state of the grocery list item to checked`, () => {
-      return test.put(`/api/grocery-list/${_id}/check`).expect(200);
-    });
-    // eslint-disable-next-line
-    it(`should update the state of the grocery list item to unchecked`, () => {
-      return test.put(`/api/grocery-list/${_id}/uncheck`).expect(200);
-    });
-  });
+  // // eslint-disable-next-line
+  // describe(`Daily Tasks Retrieval by state (checked)`, () => {
+  //   // eslint-disable-next-line
+  //   before((done) => {
+  //     let user_id = `U0U05G6JZ`;
+  //     let name = 'milk';
+  //     let state = 'checked';
+  //     test.post(`/api/grocery-list`).send({user_id, name, state}).then(() => {
+  //       done();
+  //     }).catch();
+  //   });
+  //   // eslint-disable-next-line
+  //   it(`should return a list of checked grocery list items`, () => {
+  //     return test.get(`/api/grocery-list/state/checked`).expect(200).then(({body}) => {
+  //       assert.isArray(body);
+  //       assert.equal(body[0].state, 'checked');
+  //     });
+  //   });
+  // });
+  // // eslint-disable-next-line
+  // describe(`Daily Tasks Retrieval by User`, () => {
+  //   // eslint-disable-next-line
+  //   before((done) => {
+  //     let user_id = `U0U05G6JZ`;
+  //     let name = 'milk';
+  //     let state = 'checked';
+  //     test.post(`/api/grocery-list`).send({user_id, name, state}).then(() => {
+  //       done();
+  //     }).catch();
+  //   });
+  //   // eslint-disable-next-line
+  //   it(`should return a list of grocery list items by user`, () => {
+  //     return test.get(`/api/grocery-list/user/U0U05G6JZ`).expect(200).then(({body}) => {
+  //       assert.isArray(body);
+  //       assert.equal(body[0].user_id, 'U0U05G6JZ');
+  //     });
+  //   });
+  // });
+  // // eslint-disable-next-line
+  // describe(`Daily Tasks Update`, () => {
+  //   let _id;
+  //   // eslint-disable-next-line
+  //   before((done) => {
+  //     let user_id = `U0U05G6JZ`;
+  //     let name = 'milk';
+  //     test.post(`/api/grocery-list`).send({user_id, name}).then((res) => {
+  //       _id = res.body;
+  //       done();
+  //     }).catch();
+  //   });
+  //   // eslint-disable-next-line
+  //   it(`should update the state of the grocery list item to checked`, () => {
+  //     return test.put(`/api/grocery-list/${_id}/check`).expect(200);
+  //   });
+  //   // eslint-disable-next-line
+  //   it(`should update the state of the grocery list item to unchecked`, () => {
+  //     return test.put(`/api/grocery-list/${_id}/uncheck`).expect(200);
+  //   });
+  // });
 });
