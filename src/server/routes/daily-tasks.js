2a35c9d800b72d8acbb5f46f2ec1314cb9aa2dbe
Tokyo <ahmed.tokyo1@gmail.com>
2017-03-05 01:46:50 +0200

    server:dailytasks: get tasks by day

--- src/server/routes/daily-tasks.js
+++ src/server/routes/daily-tasks.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
 const router = require('express').Router();
-// const moment = require('moment');
+const moment = require('moment');
 // const fs = require('fs');
 // const jwt = require('jwt-simple');
 
@@ -27,7 +27,35 @@ router.get('/api/daily-tasks', (req, res, next) => {
 
 /*
 |--------------------------------------------------------------------------
-| Get Daily task by user
+| Get Daily tasks by date
+| Params: date, format: 'DD-MM-YYYY'
+|--------------------------------------------------------------------------
+*/
+router.get('/api/daily-tasks/day/:date', (req, res, next) => {
+  const queryDate = moment(req.params.date, 'DD-MM-YYYY');
+  // if the date is not valid return 400
+  if(!queryDate.isValid()) {
+    return res.status(400).send({message: 'Invalid date. Required format: DD-MM-YYYY'});
+  }
+  // Find tasks on to the queryDate
+  DailyTasks.find({
+    'date': {
+      '$gte': new Date(queryDate.year(), queryDate.month(), queryDate.date()),
+      '$lt': new Date(queryDate.year(), queryDate.month(), queryDate.date()+1),
+    },
+  }).then((dailyTasks) => {
+    if (!dailyTasks) {
+      return res.status(404).send({message: 'daily tasks not found'});
+    }
+    res.send(dailyTasks);
+  }).catch((error) => {
+    next(error);
+  });
+});
+
+/*
+|--------------------------------------------------------------------------
+| Get Daily tasks by user
 |--------------------------------------------------------------------------
 */
 router.get('/api/daily-tasks/user/:id', (req, res, next) => {
@@ -47,10 +75,7 @@ router.get('/api/daily-tasks/user/:id', (req, res, next) => {
 |--------------------------------------------------------------------------
 */
 router.post('/api/daily-tasks', (req, res, next) => {
-  DailyTasks.create({
-    user_id: req.body.user_id,
-    tasks: req.body.tasks,
-  }).then((result) => {
+  DailyTasks.create({user_id: req.body.user_id, tasks: req.body.tasks}).then((result) => {
     res.status(201).send(result._id);
   }).catch((err) => {
     res.status(500).send({message: err.message});
